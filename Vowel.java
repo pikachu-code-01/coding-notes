782d28e858bfa8e56b9a71968ad79ee5478a9eff
pikachu-code-01 <devanesang5@gmail.com>
2025-08-27 16:08:06 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class vowel{
+    public static void main(String[] args) {
+        Scanner AD = new Scanner(System.in);
+        char ch = AD.next().charAt(0);
+        char newCh = Character.toUpperCase(ch);
+        if(newCh == 'A' || newCh == 'E'|| newCh == 'I'|| newCh == 'O' || newCh == 'U')
+        {
+          System.out.println("Vowel");
+        }
+        else if (Character.isLetter(newCh)){
+            System.out.println("Consonet");
+        }
+        else {
+            System.out.println("Not a letter");
+        }
+  }
+}
